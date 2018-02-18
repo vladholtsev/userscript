5ee0b1275911a34f915cbdde528d600e2b1d921d
vlad <gparadox080@gmail.com>
2018-02-18 04:45:20 +0200

    dezeer-min-0.5

--- forDeezer.js
+++ forDeezer.js
@@ -1,66 +1,25 @@
-// ==UserScript==
-// @name         for Deezer
-// @namespace    http://tampermonkey.net/
-// @include     https://www.deezer.com/*
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.deezer.com/ru/profile/1935133242/loved
-// @grant        none
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==UserScript== @name         for Deezer @namespace
+// http://tampermonkey.net/ @include     https://www.deezer.com/* @version
+// 0.1 @description  try to take over the world! @author       You @match
+// https://www.deezer.com/ru/profile/1935133242/loved @grant        none
+// @require
+// https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
 
 (function () {
     'use strict';
+    setTimeout(() => {
+        //hedi-unnecessary
+        $(".download-app").hide();
+        $(".subscribe").hide();
+        $(".svg-icon-app")
+            .parent()
+            .hide();
 
-    function start() {
-        setTimeout(() => {
-            //hedi-unnecessary
-            $(".download-app").hide();
-            $(".subscribe").hide();
-            $(".svg-icon-app").parent().hide();
+        //change-elements-form-and-style
+        $(".search").css("padding", "0 10px");
+    }, 700);
 
-            //change-elements-form-and-style
-            $(".search").css("padding", "0 10px");
-            $(".datagrid-row").css("font-weight", "900");
-            $(".navbar-link").css("font-weight", "900");
-            $(".nav-link, .profile").click(function () {});
-
-            //functions
-            $(".navbar-link, .dropdown-menu").click(function () {
-                setTimeout(() => {
-                    unstable();
-                }, 400);
-            });
-            $(".nav-link, .profile, .search-submit").click(function () {
-                start();
-            });
-            //bg-color
-            $("#menu_search").css("background-color", "#EEEEEE");
-            $(".naboo-catalog").css("background-color", "#23232C");
-            $(".container").css("background-color", "#EEEEEE");
-            $(".datagrid").css("background-color", "#EEEEEE");
-            $(".datagrid-row").hover(function () {
-                $(this).css("background-color", "#CFD8DC");
-            }, function () {
-                $(this).css("background-color", "#EEEEEE");
-            });
-
-        }, 700);
-    }
-    start();
-
-    function unstable() {
-        $(".datagrid-row").css("background-color", "#EEEEEE");
-        $(".datagrid-row").css("font-weight", "900");
-        $(".datagrid-row").hover(function () {
-            $(this).css("background-color", "#CFD8DC");
-        }, function () {
-            $(this).css("background-color", "#EEEEEE");
-        });
-    }
-
-    
     document.addEventListener('keydown', check);
     function check(e) {
         if (e.keyCode === 17) {
@@ -71,12 +30,6 @@
             $('.control-next').click();
         } else if (e.keyCode === 96) {
             $('.control-shuffle').click();
-        } else if (e.keyCode === 13) {
-            $(".navbar-link").click(function () {
-                setTimeout(() => {
-                    unstable();
-                }, 300);
-            });
         }
     }
 })();
