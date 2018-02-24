e511be330c326afddd177b5db1d1d7514f3b49c1
vlad <gparadox080@gmail.com>
2018-02-24 08:05:35 +0200

    hide-int + arrow controle

--- uscript.js
+++ uscript.js
@@ -9,7 +9,29 @@
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
+    setTimeout(() => {
+        //hedi-unnecessary
+        $(".download-app").hide();
+        $(".subscribe").hide();
+        $(".conversion-entrypoints").hide();
 
+        //change-elements-form-and-style
+        $(".search").css("padding", "0 10px");
+    }, 700);
+
+    document.addEventListener('keydown', check);
+
+    function check(e) {
+        if (e.keyCode === 38) {
+            $('.control-repeat').click(); /*up arrow*/
+        } else if (e.keyCode === 37) {
+            $('.control-prev').click(); /*right arrow*/
+        } else if (e.keyCode === 39) {
+            $('.control-next').click(); /*left arrow*/
+        } else if (e.keyCode === 40) {
+            $('.control-shuffle').click(); /*down arrow*/
+        }
+    }
 })();
