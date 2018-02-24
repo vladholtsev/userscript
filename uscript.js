ecb69329d9d9da1e992c201f0cef69355104fbf0
vlad <gparadox080@gmail.com>
2018-02-24 07:51:56 +0200

    starting point

---
+++ uscript.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         uscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  starting point
+// @author       vladyslav.holtsev@gmail.com
+// @match        https://*
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+})();
