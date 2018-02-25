f4fb69004b2045f578a34a080b277922fc65683a
vlad <gparadox080@gmail.com>
2018-02-25 22:14:27 +0200

    hide unnecessary

--- uscript.js
+++ uscript.js
@@ -1,15 +1,23 @@
 // ==UserScript==
-// @name         uscript
+// @name         youtube-minimization
 // @namespace    http://tampermonkey.net/
+// @include      https://www.youtube.com/*
 // @version      0.1
 // @description  starting point
 // @author       vladyslav.holtsev@gmail.com
-// @match        https://*
+// @match        https://www.youtube.com/*
 // @grant        none
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
+    $('document').ready(main());
 
+    function main() {
+        $("#sections ytd-guide-section-renderer:not(:nth-child(3))").hide();
+        $("#footer").hide();
+        //$("#sections ytd-guide-section-renderer:nth-child(3) ytd-guide-collapsible-entry-renderer a").css("background-color", "red");
+        //$("#sections ytd-guide-section-renderer:nth-child(3) ytd-guide-collapsible-entry-renderer a").click();
+    }
 })();
