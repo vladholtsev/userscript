e17bdc20d665fc8417ce47fd97a503c6abed7fc1
vlad <gparadox080@gmail.com>
2018-02-26 11:14:26 +0200

    add switcher

--- uscript.js
+++ uscript.js
@@ -12,12 +12,53 @@
 
 (function () {
     'use strict';
-    $('document').ready(main());
+    let newCSS = GM_getResourceText("customCSS");
+    GM_addStyle(newCSS);
 
-    function main() {
+    let switcher = true;
+    let button = $('<button>');
+    $('document').ready(check(switcher));
+
+    function check(switcher) {
+        $("#sections ytd-guide-section-renderer:last-child").after(addButton());
+    }
+
+    function addButton() {
+        // if ($("#swichButton") === null) {}
+
+        button.attr("id", "swichButton");
+        button.addClass("mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent");
+        button.text("Disable");
+        hide();
+        button.click(function () {
+            swichButton();
+        });
+        return button;
+    }
+
+    function hide() {
         $("#sections ytd-guide-section-renderer:not(:nth-child(3))").hide();
         $("#footer").hide();
         //$("#sections ytd-guide-section-renderer:nth-child(3) ytd-guide-collapsible-entry-renderer a").css("background-color", "red");
         //$("#sections ytd-guide-section-renderer:nth-child(3) ytd-guide-collapsible-entry-renderer a").click();
+    }
+
+    function show() {
+        $("#sections ytd-guide-section-renderer:not(:nth-child(3))").show();
+        $("#footer").show();
+    }
+
+
+
+    function swichButton() {
+        if (switcher === true) {
+            switcher = false;
+            button.text("Enable");
+            show();
+        } else {
+            switcher = true;
+            button.text("Disable");
+            hide();
+        }
     }
 })();
