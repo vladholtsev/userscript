6d2b16710d25dbed953590dab6d1d2b476a7e6f7
vlad <gparadox080@gmail.com>
2018-02-27 22:30:26 +0200

    del unnes

--- uscript.js
+++ uscript.js
@@ -11,54 +11,52 @@
 // ==/UserScript==
 
 (function () {
-    'use strict';
-    let newCSS = GM_getResourceText("customCSS");
-    GM_addStyle(newCSS);
+	'use strict';
+	let newCSS = GM_getResourceText("customCSS");
+	GM_addStyle(newCSS);
 
-    let switcher = true;
-    let button = $('<button>');
-    $('document').ready(check(switcher));
+	let switcher = true;
+	let button = $('<button>');
+	$('document').ready(check(switcher));
 
-    function check(switcher) {
-        $("#sections ytd-guide-section-renderer:last-child").after(addButton());
-    }
+	function check() {
+		$("#sections ytd-guide-section-renderer:last-child").after(addButton());
+	}
 
-    function addButton() {
-        // if ($("#swichButton") === null) {}
+	function addButton() {
+		// if ($("#swichButton") === null) {}
 
-        button.attr("id", "swichButton");
-        button.addClass("mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent");
-        button.text("Disable");
-        hide();
-        button.click(function () {
-            swichButton();
-        });
-        return button;
-    }
+		button.attr("id", "swichButton");
+		button.addClass("mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent");
+		button.text("Disable");
+		hide();
+		button.click(function () {
+			swichButton();
+		});
+		return button;
+	}
 
-    function hide() {
-        $("#sections ytd-guide-section-renderer:not(:nth-child(3))").hide();
-        $("#footer").hide();
-        //$("#sections ytd-guide-section-renderer:nth-child(3) ytd-guide-collapsible-entry-renderer a").css("background-color", "red");
-        //$("#sections ytd-guide-section-renderer:nth-child(3) ytd-guide-collapsible-entry-renderer a").click();
-    }
+	function hide() {
+		$("#sections ytd-guide-section-renderer:not(:nth-child(3))").hide();
+		$("#footer").hide();
+		//$("#sections ytd-guide-section-renderer:nth-child(3) ytd-guide-collapsible-entry-renderer a").css("background-color", "red");
+		//$("#sections ytd-guide-section-renderer:nth-child(3) ytd-guide-collapsible-entry-renderer a").click();
+	}
 
-    function show() {
-        $("#sections ytd-guide-section-renderer:not(:nth-child(3))").show();
-        $("#footer").show();
-    }
+	function show() {
+		$("#sections ytd-guide-section-renderer:not(:nth-child(3))").show();
+		$("#footer").show();
+	}
 
-
-
-    function swichButton() {
-        if (switcher === true) {
-            switcher = false;
-            button.text("Enable");
-            show();
-        } else {
-            switcher = true;
-            button.text("Disable");
-            hide();
-        }
-    }
+	function swichButton() {
+		if (switcher === true) {
+			switcher = false;
+			button.text("Enable");
+			show();
+		} else {
+			switcher = true;
+			button.text("Disable");
+			hide();
+		}
+	}
 })();
